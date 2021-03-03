c87a8fbf25577dc282ccdab81d973fe3ebddd823
Lovisa Bohman <lovisabo@hotmail.com>
2021-03-03 21:29:47 +0100

    Add routes for register and login, add some handling for errors

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const User = require("../model/user");
+
 const router = express.Router();
 
+const errors = " ";
 router.get("/register", (req, res) =>{
-    res.render("register.ejs");
+    res.render("register.ejs", { errors });
 });
 
-router.post("/register", (req, res)=> {
-    res.send("Submit");
+// hash password!
+router.post("/register", async (req, res) => {
+    const errors = [];
+    if(!req.body.name || !req.body.password) {
+        errors.push("Field is required");
+        //or a success screen with link/button to login?
+        res.render("error.ejs", { errors });
+    }
+
+    const user = await new User({
+        name: req.body.name,
+        password: req.body.password
+    }).save();
+    res.redirect("/");
+});
+
+router.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+router.post("/login", async (req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+
+    const username = await User.find({name:name});
+    const userPassword = await User.find({password:password});
+
+    if(username[0].name === name && userPassword[0].password === password) {
+        res.send("Inloggning lyckades!");
+    }
+
+    res.send("Inloggningen misslyckades, försök igen.");
 });
 
 module.exports = router;
