10449bbc87956249944a2c50884f455a7e24e150
Lovisa Bohman <lovisabo@hotmail.com>
2021-03-07 12:20:12 +0100

    Small change to userVerify.js

--- middleware/userVerify.js
+++ middleware/userVerify.js
@@ -4,10 +4,9 @@ require("dotenv").config();
 const verifyToken = (req, res, next) => {
     const token = req.cookies.jwtToken;
     
-    if(!token) return res.render("You're not authenticated.");
+    if(!token) return res.send("You're not authenticated.");
 
     const verified = jwt.verify(token, process.env.SECRETKEY);
-    console.log(verified);
     req.header = verified;
 
     next();
